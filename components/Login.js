5d954f31944ab0d861fc9177b928d5f4e8106d34
NiraphatK <niraphat23@gmail.com>
2023-07-22 11:49:42 +0700

    TextInputPractice

--- components/Login.js
+++ components/Login.js
@@ -1,55 +1,56 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 
 const Login = () => {
-    const [textInputName,setTextInputName] = useState('')
-    const [textInputEmail,setTextInputEmail] = useState('')
-    const checkTextInput = () => {
-        if (!textInputName.trim()){
-            alert('Please Enter Name')
-            return
-        }
-        if (!textInputEmail.trim()){
-            alert('Please Enter Email')
-            return
-        }
-        alert('Success')  
+  const [textInputName, setTextInputName] = useState("");
+  const [textInputEmail, setTextInputEmail] = useState("");
+  const checkTextInput = () => {
+    if (!textInputName.trim()) {
+      alert("Please Enter Name");
+      return;
     }
+    if (!textInputEmail.trim()) {
+      alert("Please Enter Email");
+      return;
+    }
+    alert("Success");
+  };
   return (
-    <View style = {styles.container}>
+    <View style={styles.container}>
       <TextInput
-      placeholder='Enter Name'
-      style = {styles.textInputStyle}
-      value={textInputName}
-      onChangeText={(value)=>{setTextInputName(value)}}
+        placeholder="Enter Name"
+        style={styles.textInputStyle}
+        value={textInputName}
+        onChangeText={(value) => {
+          setTextInputName(value);
+        }}
       />
-        <TextInput
-      placeholder='Enter Email'
-      style = {styles.textInputStyle}
-      value={textInputEmail}
-      onChangeText={(value)=>{setTextInputEmail(value)}}
+      <TextInput
+        placeholder="Enter Email"
+        style={styles.textInputStyle}
+        value={textInputEmail}
+        onChangeText={(value) => {
+          setTextInputEmail(value);
+        }}
       />
-      <Text>{'\n'}</Text>
-      <Button
-      title='SUBMIT'
-      onPress={checkTextInput}
-      />
+      <Text>{"\n"}</Text>
+      <Button title="SUBMIT" onPress={checkTextInput} />
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:10
-    },
-    textInputStyle:{
-        width:'100%',
-        height:40,
-        paddingHorizontal:5,
-        borderWidth:0.5,
-        marginTop:15
-    }
-})
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  textInputStyle: {
+    width: "100%",
+    height: 40,
+    paddingHorizontal: 5,
+    borderWidth: 0.5,
+    marginTop: 15,
+  },
+});
