2868637de596dddf2a5f143a57b530706a4ebdb7
NiraphatK <niraphat23@gmail.com>
2023-09-02 10:20:02 +0700

    First Commit [Lecture9-September-02-2023]

---
+++ App2.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import RandomUserScreen from './components/RandomUserScreen.js'
+import FlatList_Example1 from './components/FlatList_Example1.js'
+import FlatList_HeaderFooter from "./components/FlatList_HeaderFooter.js";
+import FlastListAPi from "./components/FlastListAPi.js";
+import News from "./components/News.js";
+import ProductScreen from "./components/ProductScreen.js";
+
+
+
+export default function App() {
+  return (
+    <View>
+      {/* <RandomUserScreen/> */}
+      {/* <FlatList_Example1/> */}
+      {/* <FlatList_HeaderFooter/> */}
+      {/* <FlastListAPi/> */}
+      {/* <News/> */}
+      {/* <ProductScreen/> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- App_StackScreen.js
+++ App_StackScreen.js
@@ -2,8 +2,26 @@ import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeScreen from './screens/HomeScreen'
-import DetailsScreen from './screens/DetailsScreen'
+
+function HomeScreen({navigation}){
+  return(
+    <View style ={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+      title='Go to Details'
+      onPress={()=>navigation.navigate('Details')}
+      />
+    </View>
+  )
+}
+
+function DetailsScreen(){
+  return(
+    <View style ={{flex:1,alignItems:'center',justifyContent:'center'}}>
+    <Text>Detail Screen</Text>
+    </View>
+  )
+}
 
 const Stack = createNativeStackNavigator()
 const App = () => {
